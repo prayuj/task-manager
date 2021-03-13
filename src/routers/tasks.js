7362689eb29ba7d57de6998d18b1dd18d622d381
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-03-13 11:58:03 +0530

    Refactored code to use express routers

---
+++ src/routers/tasks.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router = new express.Router()
+const Task = require('../model/task.model')
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findById(_id)
+        if (!task)
+            return res.status(404).send()
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    const _id = req.params.id
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        if (!task) {
+            return res.send(404).send()
+        }
+        res.status(200).send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
---
+++ src/routers/users.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const router = new express.Router()
+const User = require('../model/user.model')
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if (!user)
+            return res.status(404).send()
+        res.send(user)
+    } catch (error) {
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    const _id = req.params.id
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+    else {
+        try {
+            const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+            if (!user) {
+                return res.status(404).send()
+            }
+            res.send(user)
+
+        } catch (error) {
+            return res.status(400).send(error)
+        }
+    }
+
+})
+
+router.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        if (!user) {
+            return res.send(404).send()
+        }
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
