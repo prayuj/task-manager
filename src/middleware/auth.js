8340504b9739b5aaf52caa901ad6f8debba659c2
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-03-14 10:31:40 +0530

    Added middleware and authentication

---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const User = require('../model/user.model')
+
+const auth = async (req, res, next) => {
+    try {
+        if (!req.header('Authorization')) {
+            return res.status(400).send('Missing Bearer Token')
+        }
+
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisismynewcourse')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if (!user) {
+            throw new Error()
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        res.status(401).send({ error: 'Please authenticate.' })
+    }
+}
+
+module.exports = auth
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,12 +1,14 @@
 const express = require('express')
-const router = new express.Router()
 const User = require('../model/user.model')
+const auth = require('../middleware/auth')
+const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (error) {
         res.status(400).send(error)
     }
@@ -15,19 +17,15 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (error) {
         res.status(400).send()
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (error) {
-        res.status(500).send(error)
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
