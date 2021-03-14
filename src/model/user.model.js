1adbd891475f234910b01e286b4bb340e20621f4
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-03-14 12:23:11 +0530

    Remove all tasks of deleted user

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task.model')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -90,6 +91,14 @@ userSchema.pre('save', async function (next) {
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
+
+    next()
+})
+
+userSchema.pre('remove', async function (next) {
+    const user = this
+
+    await Task.deleteMany({ owner: user._id })
 
     next()
 })
