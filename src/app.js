ecc0d3222d1b7a9163ffe933c422858d2e45137a
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-04-25 17:11:50 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors')
 const userRouter = require('./routers/users')
 const taskRouter = require('./routers/tasks')
 
@@ -6,6 +7,7 @@ require('./db/mongoose')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
